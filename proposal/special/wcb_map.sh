f00e2fb136cfcd021f89e4c7dbfe0e44b4bd8dfb
William Waites <wwaites@tardis.ed.ac.uk>
2014-04-24 09:27:07 +0100

    cosmetic

--- proposal/special/wcb_map.sh
+++ proposal/special/wcb_map.sh
@@ -13,17 +13,17 @@ d.rast map=raster_250k
 colour2=cyan
 colour2l=blue
 colour3=green
-colour3l=orange
+colour3l=red
 colour4=yellow
 colour4l=yellow
 
-d.vect map=ptp_grass type=line display=shape color=${colour2l} width=3 where="cat_=2"
-d.vect map=ptp_grass type=line display=shape color=${colour3l} width=3 where="cat_=3"
-d.vect map=ptp_grass type=line display=shape color=${colour4l} width=3 where="cat_=4"
+d.vect map=ptp_grass type=line display=shape color=${colour2l} width=4 where="cat_=2"
+d.vect map=ptp_grass type=line display=shape color=${colour3l} width=4 where="cat_=3"
+d.vect map=ptp_grass type=line display=shape color=${colour4l} width=4 where="cat_=4"
 
-d.vect map=site_grass type=point icon=basic/star color=red fcolor=${colour2} size=25 attrcol=name lcolor=black lsize=24 xref=center yref=bottom font=bold bcolor=gold display=shape,attr where='cat_=2'
-d.vect map=site_grass type=point icon=basic/star color=red fcolor=${colour3} size=25 attrcol=name lcolor=black lsize=24 xref=center yref=bottom font=bold bcolor=gold display=shape,attr where='cat_=3'
-d.vect map=site_grass type=point icon=basic/star color=red fcolor=${colour4} size=25 attrcol=name lcolor=black lsize=24 xref=center yref=bottom font=bold bcolor=gold display=shape,attr where='cat_=4'
+d.vect map=site_grass type=point icon=basic/star color=red fcolor=${colour2} size=40 attrcol=name lcolor=black lsize=24 xref=center yref=bottom font=bold bcolor=gold display=shape,attr where='cat_=2'
+d.vect map=site_grass type=point icon=basic/star color=red fcolor=${colour3} size=60 attrcol=name lcolor=black lsize=24 xref=center yref=bottom font=bold bcolor=gold display=shape,attr where='cat_=3'
+d.vect map=site_grass type=point icon=basic/star color=red fcolor=${colour4} size=40 attrcol=name lcolor=black lsize=24 xref=center yref=bottom font=bold bcolor=gold display=shape,attr where='cat_=4'
 
 d.barscale at=5,95
 
